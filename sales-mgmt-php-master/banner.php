6b6e8cd247470cc2e985734978193f69f311b2fd
Smith Mwivali <mwivalismith@gmail.com>
2022-02-21 18:44:11 +0300

    Initial commit

---
+++ sales-mgmt-php-master/banner.php
@@ -0,0 +1,28 @@
+<?php include("header.php");?>
+<style type="text/css">
+<!--
+.style2 {
+	color: #009933;
+	font-size: 14px;
+}
+.style3 {font-size: 30px}
+.style4 {
+	color: #000000;
+	font-weight: bold;
+}
+-->
+</style>
+
+	<div class="header">
+	<div class="title">
+	<div class="logo1"><img src="images/imagddhdhdes.jpg" height="100" width="100"/></div>
+		<div class="logo1des style2">
+			<span class="style3">AAA POWER</span>		<br>
+			<span class="style4">H # 145, Rd # 19A 
+            <br>
+            Dhanmondi, Dhaka-1209.
+		    <br>
+		    Cell: 000-000-0191<br>
+	    e-mail:something@gmail.com</span></div>
+	</div>
+	</div>
